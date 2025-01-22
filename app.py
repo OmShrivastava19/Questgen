fa1e48c7b038142e7dbba8774e5d8030f8966edb
Om Shrivastava <omshrivastava01927@gmail.com>
2025-01-22 21:21:07 +0530

    fixed update quest
    created update.html where user redirect and it has prefilled data now there user have to change and click on update and the data will be updated in the database.

--- app.py
+++ app.py
@@ -52,28 +52,42 @@ def delete(sno):
     db.session.commit()
     return redirect("/browse")
 
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method=='POST':
+        standard = request.form['standard']
+        subject = request.form['subject']
+        chapter = request.form['chapter']
+        topic = request.form['topic']
+        question = request.form['question']
+        diff = request.form['diff']
+        answer = request.form['answer']
+        quest = Quest.query.filter_by(sno=sno).first()
+        quest.standard = standard
+        quest.subject = subject
+        quest.chapter = chapter
+        quest.topic = topic
+        quest.question = question
+        quest.diff = diff
+        quest.answer = answer
+        db.session.add(quest)
+        db.session.commit()
+        return redirect("/browse")
+    # quest_id = request.args.get('id')
+    # quest = Quest.query.filter_by(id=quest_id).first()
+    # return render_template('update.html', quest=quest)
+    quest = Quest.query.filter_by(sno=sno).first()
+    return render_template('update.html', quest=quest)
+
 @app.route('/browse')
 def browse():
+    if request.method == 'POST':
+        # Handle POST logic here
+        pass
     allQuest = Quest.query.all()
     return render_template('all_quest.html', allQuest=allQuest)
-
-# @app.route('/update/<int:sno>', methods=['GET', 'POST'])
-# def update(sno):
-#     if request.method=='POST':
-#         title = request.form['title']
-#         desc = request.form['desc']
-#         quest = quest.query.filter_by(sno=sno).first()
-#         todo.title = title
-#         quest.desc = desc
-#         db.session.add(quest)
-#         db.session.commit()
-#         return redirect("/")
-        
-#     todo = Todo.query.filter_by(sno=sno).first()
-#     return render_template('update.html', todo=todo)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  
     app.run(debug=True, port=5000)
-
